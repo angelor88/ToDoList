584ef36fa4e13440b09d44f20bd8aa7d5d9f9734
angelor88 <angelorussoniello88@gmail.com>
2018-02-22 10:29:59 -0800

    Add code for program and startup.

--- Models/Item.cs
+++ Models/Item.cs
@@ -3,15 +3,15 @@ namespace ToDoList.Models
     public class Item
     {
       private string _description;
-      public Item (string _description)
+      public Item (string description)
       {
         _description = description;
       }
       public string GetDescription()
       {
-        return _description
+        return _description;
       }
-      public string SetDescrption(string newDescription)
+      public void SetDescrption(string newDescription)
        {
          _description = newDescription;
        }
