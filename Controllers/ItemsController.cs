0a0a7b076acf7e800fd7b6f9d4c802e8c7ee85b8
angelor88 <angelorussoniello88@gmail.com>
2018-02-22 11:53:31 -0800

    Add static methods and class to the project.

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
+using System.Collections.Generic;
 
 namespace ToDoList.Controllers
   {
@@ -8,12 +9,26 @@ namespace ToDoList.Controllers
         [HttpGet("/items")]
         public ActionResult Index()
         {
-          Item newItem = new Item(Request.Query["new-item"]);
-          return View(newItem);
+          List<Item> allItems = Item.GetAll();
+          return View(allItems);
         }
         [HttpGet("/items/new")]
         public ActionResult CreateForm()
         {
+          return View();
+        }
+        [HttpPost("/items")]
+        public ActionResult Create()
+        {
+          Item newItem = new Item (Resquest.Form["new-item"]);
+          newItem.Save();
+          List<Item> allItems = Item.GetAll();
+          return View("Index", allItems);
+        }
+        [HttpPost("/items/delete")]
+        public ActionResult DeleteAll()
+        {
+          Item.ClearAll();
           return View();
         }
       }
